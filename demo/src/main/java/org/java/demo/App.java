35ef90c28c4b91bf315f59840064fc9e27e3d31d
Jan Bełczącki <Jan Beczcki>
2017-02-28 16:13:27 +0100

    Maven with jUnit4 works .gitignore for target and .idea folders

---
+++ demo/src/main/java/org/java/demo/App.java
@@ -0,0 +1,8 @@
+package org.java.demo;
+
+/**
+ * Created by janbe on 28.02.2017.
+ */
+public class App {
+
+}
---
+++ demo/src/test/java/org/java/demo/AppTest.java
@@ -0,0 +1,16 @@
+package org.java.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by janbe on 28.02.2017.
+ */
+public class AppTest {
+    @Test
+    public void AssertTest()
+    {
+        assertTrue(true);
+    }
+}
